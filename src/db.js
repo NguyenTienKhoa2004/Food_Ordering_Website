c6cc728cbe4374762eae21f9c12b678548878172
minhtri1612 <10422119@student.vgu.edu.vn>
2025-06-19 19:08:50 +0700

    done docker

--- src/db.js
+++ src/db.js
@@ -1,10 +1,27 @@
+// const mysql = require('mysql2/promise');
+
+// const dbConfig = {
+//     host: 'localhost',
+//     user: 'root',          // Replace with your MySQL username
+//     password: 'Minhchau3112...', // Replace with your MySQL password
+//     database: 'foodapp'
+// };
+
+// async function getConnection() {
+//     return await mysql.createConnection(dbConfig);
+// }
+
+// module.exports = { getConnection };
+
+
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',          // Replace with your MySQL username
-    password: '123456', // Replace with your MySQL password
-    database: 'foodapp'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 };
 
 async function getConnection() {
